ddef322462313316baf59a5ecf9828eefdf37395
Deekshith SH <shdeekshith@gmail.com>
2022-04-12 06:50:30 +0530

    Update __main__.py

--- Test/__main__.py
+++ Test/__main__.py
@@ -1,14 +1,13 @@
 import asyncio, sys
 from pyrogram import idle
 from Test.Bot import TelegramBot
-import uvloop
-uvloop.install()
 
-if sys.version_info[1] > 9:
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-else:
-    loop = asyncio.get_event_loop()
+
+
+# loop = asyncio.new_event_loop()
+# asyncio.set_event_loop(loop)
+
+loop = asyncio.get_event_loop()
 
 async def main():
     await TelegramBot.start()
