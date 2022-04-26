06337894cc7f52c2a61193a7dc7d9d2c8762ab44
Deekshith SH <shdeekshith@gmail.com>
2022-04-26 22:20:03 +0530

    Update start.py

--- Test/plugins/start.py
+++ Test/plugins/start.py
@@ -1,7 +1,7 @@
 from Test.Bot import TelegramBot
 from pyrogram import filters
 
-@TelegramBot.on_message(filters.private & filters.text)
+@TelegramBot.on_message(filters.private)
 async def test(b, m):
     print(m.text)
-    await m.reply_text(m.text)
+    await m.reply_text("Use @tgxlinkbot insted of this bot\nby @DeekshithSH")
